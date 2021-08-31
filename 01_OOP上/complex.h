66dfca4dbef89d674a3701b4ff044e1b37618279
LeoSirius <is.li.xiaoyu@qq.com>
2021-08-31 19:15:46 +0800

    update

--- 01_OOP上/complex.h
+++ 01_OOP上/complex.h
@@ -12,10 +12,146 @@ public:
     complex& operator+=(const complex&);
     complex& operator-=(const complex&);
     complex& operator*=(const complex&);
-    complex& operator/=(const complex&);
+    double real() const { return re; }
+    double imag() const { return im; }
 private:
     double re, im;
 
+    friend complex& __doapl(complex *, const complex&);
+    friend complex& __doami(complex *, const complex&);
+    friend complex& __doaml(complex *, const complex&);
 };
+
+
+
+inline
+complex& complex::operator+=(const complex& r) {
+    return __doapl(this, r);
+}
+
+inline
+complex& complex::operator-=(const complex& r) {
+    return __doami(this, r);
+}
+
+inline
+complex& complex::operator*=(const complex& r) {
+    return __doaml(this, r);
+}
+
+inline
+double real(const complex& x) {
+    return x.real();
+}
+
+inline
+double imag(const complex& x) {
+    return x.imag();
+}
+
+inline
+complex operator+(const complex& x, const complex& y) {
+    return complex(real(x) + real(y), imag(x) + imag(y));
+}
+
+inline
+complex operator+(const complex& x, double y) {
+    return complex(real(x), imag(x) + y);
+}
+
+inline
+complex operator+(double x, const complex& y) {
+    return complex(x + real(y), imag(y));
+}
+
+inline
+complex operator-(const complex& x, const complex& y) {
+    return complex(real(x) - real(y), imag(x) - imag(y));
+}
+
+inline
+complex operator-(const complex& x, double y) {
+    return complex(real(x), imag(x) - y);
+}
+
+inline
+complex operator-(double x, const complex& y) {
+    return complex(x - real(y), imag(y));
+}
+
+inline
+complex operator*(const complex& x, const complex& y) {
+    return complex(real(x) * real(y) - imag(x) * imag(y),
+                   real(x) * imag(y) + imag(x) * real(y));
+}
+
+
+inline
+complex operator*(const complex& x, double y) {
+    return complex(real(x) * y, imag(x) * y);
+}
+
+inline
+complex operator*(double x, const complex& y) {
+    return complex(x * real(y), x * imag(y));
+}
+
+complex operator/(const complex& x, double y) {
+    return complex(real(x) / y, imag(x) / y);
+}
+
+inline
+complex operator+(const complex& x) {
+    return x;
+}
+
+inline
+complex operator-(const complex& x) {
+    return complex(-real(x), -imag(x));
+}
+
+inline
+bool operator==(const complex& x, const complex& y) {
+    return real(x) == real(y) && imag(x) == imag(y);
+}
+
+inline
+bool operator==(const complex& x, double y) {
+    return real(x) == y && imag(x) == 0;
+}
+
+inline
+bool operator==(double x, const complex& y) {
+    return x == real(y) && imag(y) == 0;
+}
+
+inline
+bool operator!=(const complex& x, const complex& y) {
+    return real(x) != real(y) || imag(x) != imag(y);
+}
+
+inline
+bool operator!=(const complex& x, double y) {
+    return real(x) != y || imag(x) != 0;
+}
+
+inline
+bool operator!=(double x, const complex& y) {
+    return x != real(y) || imag(y) != 0;
+}
+
+
+
+
+
+
+
+
+inline
+complex& __doapl(complex* ths, const complex& r) {
+    ths->re += r.re;
+    ths->im += r.im;
+    return *ths;
+}
 
 #endif
